1cc42b541c8ecca474fb6eef43e3c86883628bd6
popipuppy <105240901+popipuppy@users.noreply.github.com>
2025-03-07 09:35:30 +0100

    lektion-2-del-3

--- server/routes/postsRoute.js
+++ server/routes/postsRoute.js
@@ -1,7 +1,35 @@
 const router = require('express').Router();
+const db = require ('../models');
+
 
 router.get('/', (req, res) => {
-    res.send("Get posts");
+    db.post.findAll().then((result) => {
+        res.send(result);
+    });
 });
+
+router.post('/', (req, res) => {
+    db.post.create(req.body).then(result => {
+        res.send(result);
+    });
+});
+router.put('/', (req, res) => {
+    db.post
+        .update(req.body,{
+        where: { id: req.body.id }
+    }).then(result => {
+        res.send(result);
+    });
+});
+router.delete('/', (req, res) => {
+    db.post
+        .destroy({
+        where: { id: req.body.id }
+    }).then(result => {
+        res.json({ message: `Inlägget raderades: ${result}` });
+    });
+});
+
+
 
 module.exports = router;
